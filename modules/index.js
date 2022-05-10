5d543441b01ce34c344a52fbf66a8f7a1c634ebd
Stephen <stephen.s.espinosa@gmail.com>
2022-05-10 20:40:12 +0200

    fix responsive

--- modules/index.js
+++ modules/index.js
@@ -17,26 +17,29 @@ function buttonEvents() {
 }
 
 function hover(element) {
-  console.log(`h4#${element.target.id}`);
+  console.log(element.target);
   document.querySelector(`h4#${element.target.id}`).classList.add("blur");
 
   if (`h4#${element.target.id}` == "h4#creative") {
-    document.querySelector("#container-3").classList.remove("scroll");
-    document.querySelector("#container-3").style.backgroundImage =
+    document.querySelector("#container-4").classList.remove("scroll");
+    document.querySelector("#container-4").style.backgroundImage =
       "url('/public/assets/cube.jpg')";
-    document.querySelector("#container-3").classList.add("scroll");
+    document.querySelector("#container-4").offsetHeight;
+    document.querySelector("#container-4").classList.add("scroll");
   }
   if (`h4#${element.target.id}` == "h4#marketing") {
-    document.querySelector("#container-3").classList.remove("scroll");
-    document.querySelector("#container-3").style.backgroundImage =
+    document.querySelector("#container-4").classList.remove("scroll");
+    document.querySelector("#container-4").style.backgroundImage =
       "url('/public/assets/market.jpg')";
-    document.querySelector("#container-3").classList.add("scroll");
+    document.querySelector("#container-4").offsetHeight;
+    document.querySelector("#container-4").classList.add("scroll");
   }
   if (`h4#${element.target.id}` == "h4#development") {
-    document.querySelector("#container-3").classList.remove("scroll");
-    document.querySelector("#container-3").style.backgroundImage =
+    document.querySelector("#container-4").classList.remove("scroll");
+    document.querySelector("#container-4").style.backgroundImage =
       "url('/public/assets/grow.jpg')";
-    document.querySelector("#container-3").classList.add("scroll");
+    document.querySelector("#container-4").offsetHeight;
+    document.querySelector("#container-4").classList.add("scroll");
   }
 }
 
