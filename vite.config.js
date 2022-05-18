ad505a31b0092c5e3b083dac32a60e2139767a13
Stephen <stephen.s.espinosa@gmail.com>
2022-05-18 12:25:37 +0200

    vite config added pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,10 @@ module.exports = {
       input: {
         main: resolve(__dirname, "index.html"),
         sub0: resolve(__dirname, "cases.html"),
+        sub1: resolve(__dirname, "approach.html"),
+        sub2: resolve(__dirname, "services.html"),
+        sub3: resolve(__dirname, "team.html"),
+        sub4: resolve(__dirname, "contact.html"),
       },
     },
   },
